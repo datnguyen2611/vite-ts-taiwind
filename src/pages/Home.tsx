73befa417dacbda6fc403b60a00c7237bddbfc6f
dat nguyen <datnguyen26111997@gmail.com>
2024-04-09 10:23:27 +0700

    remove home game

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <iframe
-        src="/src/game/SlashTheHordes/index.html"
-        width="100%"
-        height="500"
-        title="Cocos Game"
-      ></iframe>
+      <p>home</p>
     </div>
   );
 };
